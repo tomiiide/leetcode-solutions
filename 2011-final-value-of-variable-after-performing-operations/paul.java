7f2a4c6554e40d9b4702d4a68edf085daeb1ca43
Paul Okeke <donpaul120@gmail.com>
2022-02-07 22:12:52 +0100

    Solution by Paul

---
+++ 2011-final-value-of-variable-after-performing-operations/paul.java
@@ -0,0 +1,15 @@
+/*
+* Simple Loop and checks
+*/
+
+public int finalValueAfterOperations(String[] operations) {
+  int x = 0;
+  for(int i = 0; i < operations.length; i++) {
+    if(operations[i].equals("++X") || operations[i].equals("X++")) {
+      x++;
+    } else {
+      x--;
+    }
+  }
+  return x;
+}
